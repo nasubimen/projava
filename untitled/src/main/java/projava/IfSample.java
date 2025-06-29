d54d522b8d4bee69825646afe760f0b8ac0151e9
RENTAROUPC\renta <rentarouclass@gmail.com>
2025-06-29 10:59:01 +0900

    練習用ファイル

---
+++ untitled/src/main/java/projava/IfSample.java
@@ -0,0 +1,12 @@
+package main.java.projava;
+
+public class IfSample {
+    public static void main(String[] args) {
+        var a = 5;
+        if(a < 3){
+            System.out.println("小さい");
+        }else{
+            System.out.println("それ以外の値");
+        }
+    }
+}
---
+++ untitled/src/main/java/projava/SwitchSample.java
@@ -0,0 +1,5 @@
+package main.java.projava;
+
+public class SwitchSample {
+    
+}
